ab7afe3de8ccbc091d0f4f3b2a0aa7b20e90974c
horden316 <horden316@gmail.com>
2024-09-25 16:37:14 +0800

    add patient_sex

--- worklists_generate.py
+++ worklists_generate.py
@@ -3,26 +3,28 @@ from pydicom.dataset import Dataset, FileDataset
 from pydicom.uid import generate_uid, ExplicitVRLittleEndian
 from pynetdicom import AE, evt, StoragePresentationContexts, VerificationPresentationContexts, QueryRetrievePresentationContexts
 
-def create_modality_worklist(patient_name, patient_id, accession_number, study_instance_uid):
+def create_modality_worklist(patient_name, patient_id, accession_number, study_instance_uid, patient_sex):
     # 設定DICOM標籤和值
     ds = Dataset()
     ds.PatientName = patient_name
     ds.PatientID = patient_id
     ds.AccessionNumber = accession_number
     ds.StudyInstanceUID = study_instance_uid
+    ds.PatientSex = patient_sex  # 新增PatientSex標籤
     ds.Modality = 'CT'
     ds.ScheduledProcedureStepSequence = [Dataset()]
-    ds.ScheduledProcedureStepSequence[0].ScheduledStationAETitle = 'STATION_AE'
+    ds.ScheduledProcedureStepSequence[0].ScheduledStationAETitle = 'LELTEK'
     ds.ScheduledProcedureStepSequence[0].ScheduledProcedureStepStartDate = datetime.date.today().strftime('%Y%m%d')
     ds.ScheduledProcedureStepSequence[0].ScheduledProcedureStepStartTime = datetime.datetime.now().strftime('%H%M%S')
-    ds.ScheduledProcedureStepSequence[0].ScheduledPerformingPhysicianName = 'Dr. Who'
-    ds.ScheduledProcedureStepSequence[0].ScheduledProcedureStepDescription = 'CT Brain'
+    ds.ScheduledProcedureStepSequence[0].ScheduledPerformingPhysicianName = '' # 醫生名字
+    ds.ScheduledProcedureStepSequence[0].ScheduledProcedureStepDescription = '' # 檢查描述(例如：胸部CT)
 
     # 設定文件訊息
     file_meta = Dataset()
     file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.31'
     file_meta.MediaStorageSOPInstanceUID = generate_uid()
     file_meta.ImplementationClassUID = '1.2.3.4.5.6.7.8.9.0'  # 固定的UID
+    file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
 
     # 創建DICOM文件
     filename = f'MWL_{patient_id}.wl'
@@ -50,6 +52,6 @@ def start_scp():
 
 if __name__ == "__main__":
     # 示例用法
-    mwl = create_modality_worklist('John Doe', '123456', '654321', generate_uid())
+    #mwl = create_modality_worklist('John Doe', '123456', '654321', generate_uid(), 'M') # 新增性別參數
     # 啟動SCP
     start_scp()
