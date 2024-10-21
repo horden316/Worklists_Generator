905177b647a95b7d7e424fcf5fff9d183e5097aa
horden316 <horden316@gmail.com>
2024-10-21 20:40:13 +0800

    fix: remove study_instance_uid in API_worklists_generate.py

--- API_worklists_generate.py
+++ API_worklists_generate.py
@@ -12,8 +12,8 @@ class WorklistRequest(BaseModel):
     patient_name: str
     patient_id: str
     accession_number: str
-    study_instance_uid: str
     patient_sex: str
+    patient_birth_date: str
 
 # API 端點：接收資料並呼叫你的 Script
 @app.post("/create_worklist/")
@@ -23,12 +23,11 @@ async def create_worklist(worklist: WorklistRequest):
         patient_name = worklist.patient_name
         patient_id = worklist.patient_id
         accession_number = worklist.accession_number
-        study_instance_uid = worklist.study_instance_uid
         patient_sex = worklist.patient_sex
 
         # 呼叫 Script 的函數，或者你也可以選擇用 subprocess 來呼叫外部 Script
         from worklists_generate import create_modality_worklist
-        create_modality_worklist(patient_name, patient_id, accession_number, study_instance_uid, patient_sex)
+        create_modality_worklist(patient_name, patient_id, accession_number, patient_sex)
 
         # .wl 檔案的路徑（假設檔案名與 patient_id 有關）
         filename = f'MWL_{patient_id}.wl'
